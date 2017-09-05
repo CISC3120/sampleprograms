9aeb2c04bd3da6fee32e223ed883ad826afa1f44
Gray Chan <gray.chenhui@gmail.com>
2017-09-05 16:18:33 -0400

    revised the BeerSong with command line arguments

--- BeerSong/BeerSong.java
+++ BeerSong/BeerSong.java
@@ -7,6 +7,20 @@
 public class BeerSong {
     public static void main (String[] args) {
         int beerNum = 99;
+        int takeDownNum = 1; 
+
+        /* 
+         * add command line arguments to set starting bottles of
+         * beer and the bottles of beer to take down. However, this
+         * revision introduces two bugs to the program. What are they?
+         * */
+        if (args.length == 1) {
+            beerNum = Integer.parseInt(args[0]);
+        } else if (args.length >= 2) {
+            beerNum = Integer.parseInt(args[0]);
+            takeDownNum = Integer.parseInt(args[1]);
+        }
+
         String word = "bottles";
         
         while (beerNum > 0 ) {
@@ -18,7 +32,7 @@ public class BeerSong {
             System.out.println(beerNum + " " + word + " of beer.");
             System.out.println("Take one down.");
             System.out.println("Pass it around.") ;
-            beerNum --;
+            beerNum -= takeDownNum;
             
             if (beerNum > 0) {
                 System.out.println(beerNum + " " + word + " of beer on the wall");
