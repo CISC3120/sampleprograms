b5a504847e87117cd1f142da92dc91c1f33a84c5
Gray Chan <gray.chenhui@gmail.com>
2017-09-05 12:06:08 -0400

    added the Guess Game example

---
+++ GuessGame/src/main/java/CISC3120/GuessGame/GameLauncher.java
@@ -0,0 +1,8 @@
+package CISC3120.GuessGame;
+
+public class GameLauncher {
+    public static void main (String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
