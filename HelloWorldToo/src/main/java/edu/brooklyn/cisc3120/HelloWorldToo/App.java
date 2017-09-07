169fe5c19bf5c51772b5ed4da98b78f8efe4992c
Hui Chen <hui.chen@brooklyn.cuny.edu>
2017-09-07 15:13:49 -0400

    Hello, World too

---
+++ HelloWorldToo/src/main/java/edu/brooklyn/cisc3120/HelloWorldToo/App.java
@@ -0,0 +1,11 @@
+package edu.brooklyn.cisc3120.HelloWorldToo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
