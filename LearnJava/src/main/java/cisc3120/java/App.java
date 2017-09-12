70a1e70849a8b4a034148791d11d38692b86ac76
Gray Chan <gray.chenhui@gmail.com>
2017-09-12 14:09:18 -0400

    added break and continue examples

--- LearnJava/src/main/java/cisc3120/java/App.java
+++ LearnJava/src/main/java/cisc3120/java/App.java
@@ -106,7 +106,37 @@ public class App
 //			default:
 //				System.out.println("consonant");
 //			}
-//    	}    	
+//    	}   
+    	
+    	
+//		for (int i = 0; i < 100; i++) {
+//			if (i == 74)
+//				break; // Out of for loop
+//			if (i % 9 != 0)
+//				continue; // Next iteration
+//			System.out.print(i + " ");
+//		}
+//		System.out.println();
+//		for (int i = 0; i < 100; i++) {
+//			if (i == 74)
+//				break; // Out of for loop
+//			if (i % 9 != 0)
+//				continue; // Next iteration
+//			System.out.print(i + " ");
+//		}
+//		System.out.println();
+//		int i = 0;
+//		// An "infinite loop":
+//		while (true) {
+//			i++;
+//			int j = i * 27;
+//			if (j == 1269)
+//				break; // Out of loop
+//			if (i % 10 != 0)
+//				continue; // Top of loop
+//			System.out.print(i + " ");
+//		}
+    		
     }
     
 	static boolean test1(int val) {
