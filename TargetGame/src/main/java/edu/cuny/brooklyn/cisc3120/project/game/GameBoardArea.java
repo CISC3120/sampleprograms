8ecd167824c141c5de29bc51d36d467d96365eb3
Gray Chan <gray.chenhui@gmail.com>
2017-09-14 08:26:30 -0400

    added project 1 start-up code

---
+++ TargetGame/src/main/java/edu/cuny/brooklyn/cisc3120/project/game/GameBoardArea.java
@@ -0,0 +1,13 @@
+package edu.cuny.brooklyn.cisc3120.project.game;
+
+public class GameBoardArea {
+//	private int width;
+//	private int height;
+//	private int[][] cells;
+//	
+//	public GameBoardArea(/* TODO */) {
+//		// TODO
+//	}
+//
+//	// TODO: more ...
+}
---
+++ TargetGame/src/main/java/edu/cuny/brooklyn/cisc3120/project/game/TargetGame.java
@@ -0,0 +1,53 @@
+package edu.cuny.brooklyn.cisc3120.project.game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TargetGame {
+	private static Logger logger = LoggerFactory.getLogger(TargetGame.class);
+	
+	private final int GAME_TARGET_AREA_WIDTH = 80;
+	private final int GAME_TARGET_AREA_HEIGHT = 25;
+
+	GameBoard gameBoard;      // having its own dimension: cells
+	GameDisplay gameDisplay;  // having its own dimension: cells to characters
+	private Random rng;
+	private Scanner in;
+	
+	public TargetGame() {
+		gameBoard = new GameBoard(GAME_TARGET_AREA_HEIGHT, GAME_TARGET_AREA_WIDTH);
+		gameDisplay = new GameDisplay(gameBoard);
+		rng = new Random();
+		in = new Scanner(System.in);
+	}
+	
+	public void play() {
+		setTarget();
+		gameBoard.plotBorder();
+		gameBoard.writeText(0, GAME_TARGET_AREA_HEIGHT-1, "Enter your target position (x, y):");
+		while(true) {
+			gameDisplay.draw();
+
+			int xGuess = in.nextInt();
+			int yGuess = in.nextInt();
+			logger.debug("Player guessed x = " + xGuess + ", y =" + yGuess + ".");
+			if (gameBoard.getCell(xGuess, yGuess) == 'X') {
+				gameBoard.writeText(0, GAME_TARGET_AREA_HEIGHT-1, "You won");
+			} else {
+				gameBoard.writeText(0, GAME_TARGET_AREA_HEIGHT-1,"Try again. Enter your target position (x, y): ");
+			}
+			gameDisplay.draw();
+		}
+	}
+	
+	private void setTarget() {
+		int x = rng.nextInt(GAME_TARGET_AREA_WIDTH);
+		int y = rng.nextInt(GAME_TARGET_AREA_HEIGHT);
+		gameBoard.setCell(x, y, 'X');
+		logger.debug("Target: " + x + "," + y);
+	}
+	
+}
