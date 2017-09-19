434d453491c5f741a42916b4f418a884dca8e566
Gray Chan <gray.chenhui@gmail.com>
2017-09-19 16:17:16 -0400

    updated the Array and Array List with tests

--- ArrayArrayList/src/test/java/edu/cuny/brooklyn/cisc3120/FruitArrayTest.java
+++ ArrayArrayList/src/test/java/edu/cuny/brooklyn/cisc3120/FruitArrayTest.java
@@ -1,38 +1,111 @@
 package edu.cuny.brooklyn.cisc3120;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+// import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
+ * API documentation: http://junit.org/junit4/javadoc/latest/index.html
+ * Example: https://github.com/junit-team/junit4/wiki/getting-started
  */
-public class AppTest 
-    extends TestCase
+
+public class FruitArrayTest
 {
+	// TODO: 1. explain why I gave up the idea of using assertArrayEquals. It seems less code if I used it. 
+	// TODO: 2. add a test class FruitArrayListTest.java, and design and implement a few tests for the 
+	//          delete method in the FruitArrayList class
+	
+	
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+     * Test the delete method thoroughly
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+	@Test
+    public void testDeleteByDeleting1st() {
+    	// why not this: String[] fruits = {"Apple", "Banana", "Kiwi", "Mango", "Orange"};
+        String[] fruits = {new String("Apple"), new String("Banana"), new String("Kiwi"), new String("Mango"), new String("Orange")};
+        FruitArray fruitArray = new FruitArray(fruits);    
+
+        fruitArray.delete(new String("Apple"));
+        String[] fruitsExpected = {new String("Banana"), new String("Kiwi"), new String("Mango"), new String("Orange")};
+        
+        assertEquals("The size of the FruitArray object is unexpected.", fruitArray.getSize(), fruitsExpected.length);
+        assertEquals(fruitArray.getCapacity(), fruitsExpected.length + 1);
+        // why not assertArrayEquals( ... )
+		for (int i = 0; i < fruitsExpected.length; i++) {
+			assertTrue(fruitArray.getFruitsAsArray()[i].equals(fruitsExpected[i]));
+		}
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+	@Test
+    public void testDeleteByDeleting3rd() {
+    	// why not this: String[] fruits = {"Apple", "Banana", "Kiwi", "Mango", "Orange"};
+        String[] fruits = {new String("Apple"), new String("Banana"), new String("Kiwi"), new String("Mango"), new String("Orange")};
+        FruitArray fruitArray = new FruitArray(fruits);    
+        
+        fruitArray.delete(new String("Kiwi"));
+        String[] fruitsExpected = {new String("Apple"), new String("Banana"), new String("Mango"), new String("Orange")};
+
+        assertEquals("The size of the FruitArray object is unexpected.", fruitArray.getSize(), fruitsExpected.length);
+        assertEquals(fruitArray.getCapacity(), fruitsExpected.length + 1);
+        // why not assertArrayEquals( ... )
+        for (int i = 0; i < fruitsExpected.length; i++) {
+			assertTrue(fruitArray.getFruitsAsArray()[i].equals(fruitsExpected[i]));
+		}
+    }  
+
+	@Test
+    public void testDeleteByDeletingLast() {
+    	// why not this: String[] fruits = {"Apple", "Banana", "Kiwi", "Mango", "Orange"};
+        String[] fruits = {new String("Apple"), new String("Banana"), new String("Kiwi"), new String("Mango"), new String("Orange")};
+        FruitArray fruitArray = new FruitArray(fruits);    
+        
+        fruitArray.delete(new String("Orange"));
+        String[] fruitsExpected = {new String("Apple"), new String("Banana"), new String("Kiwi"), new String("Mango")};
+        
+        assertEquals("The size of the FruitArray object is unexpected.", fruitArray.getSize(), fruitsExpected.length);
+        assertEquals(fruitArray.getCapacity(), fruitsExpected.length + 1);
+        // why not assertArrayEquals( ... )        
+		for (int i = 0; i < fruitsExpected.length; i++) {
+			assertTrue(fruitArray.getFruitsAsArray()[i].equals(fruitsExpected[i]));
+		}
+    } 
+    
+	@Test
+    public void testDeleteIgnoreCaseByDeleting1st() {
+    	// why not this: String[] fruits = {"Apple", "Banana", "Kiwi", "Mango", "Orange"};
+        String[] fruits = {new String("Apple"), new String("Banana"), new String("Kiwi"), new String("Mango"), new String("Orange")};
+        FruitArray fruitArray = new FruitArray(fruits);    
+        
+        fruitArray.delete(new String("apple"));
+        String[] fruitsExpected = {new String("Banana"), new String("Kiwi"), new String("Mango"), new String("Orange")};
+
+        assertEquals("The size of the FruitArray object is unexpected.", fruitArray.getSize(), fruitsExpected.length);
+        assertEquals(fruitArray.getCapacity(), fruitsExpected.length + 1);
+        // why not assertArrayEquals( ... )        
+		for (int i = 0; i < fruitsExpected.length; i++) {
+			assertTrue(fruitArray.getFruitsAsArray()[i].equals(fruitsExpected[i]));
+		}
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+
+	@Test
+    public void testDeleteIgnoreCaseByDeletingLast() {
+    	// why not this: String[] fruits = {"Apple", "Banana", "Kiwi", "Mango", "Orange"};
+        String[] fruits = {new String("Apple"), new String("Banana"), new String("Kiwi"), new String("Mango"), new String("Orange")};
+        FruitArray fruitArray = new FruitArray(fruits);    
+        
+        fruitArray.delete(new String("orangE"));
+        String[] fruitsExpected = {new String("Apple"), new String("Banana"), new String("Kiwi"), new String("Mango")};
+
+        
+        assertEquals("The size of the FruitArray object is unexpected.", fruitArray.getSize(), fruitsExpected.length);
+        assertEquals(fruitArray.getCapacity(), fruitsExpected.length + 1);
+        // why not assertArrayEquals( ... )        
+		for (int i = 0; i < fruitsExpected.length; i++) {
+			assertTrue(fruitArray.getFruitsAsArray()[i].equals(fruitsExpected[i]));
+		}
+    }     
+    
 }
